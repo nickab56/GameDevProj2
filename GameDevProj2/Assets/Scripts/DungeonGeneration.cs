ee05161d4fa6bc442c679dbb18ad591b4d971cf1
FrozenScorpio <116769282+FrozenScorpio@users.noreply.github.com>
2022-11-23 16:03:31 -0500

    Started Generation and Add Sprites
    Start working on generation didn't realize how long this is going to take but we will get there
    Also added the player knight textures

--- GameDevProj2/Assets/Scripts/DungeonGeneration.cs
+++ GameDevProj2/Assets/Scripts/DungeonGeneration.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class DungeonGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject CornerOne;
+    public GameObject CornerTwo;
+    public GameObject CornerThree;
+    public GameObject CornerFour;
+    public GameObject FourSidesOne;
+    public GameObject FourSideTwo;
+    public GameObject FourSideThree;
+
+
+    // The Acutally Rooms
+    private GameObject RoomOne;
+    private GameObject RoomTwo;
+    private GameObject RoomThree;       //  1|2|3
+    private GameObject RoomFour;        //  4|5|6
+    private GameObject RoomFive;        //  7|8|9
+    private GameObject RoomSix;
+    private GameObject RoomSeven;
+    private GameObject RoomEight;
+    private GameObject RoomNine;
+
     void Start()
     {
-        
-    }
+        RoomFive = Instantiate(FourSidesOne);
+        RoomFive.transform.position = Vector3.zero;
+        RoomSix = Instantiate(CornerOne);
+        RoomSix.transform.position = new Vector3(9, 0, 0);
+        RoomThree = Instantiate(CornerTwo);
+        RoomThree.transform.position = new Vector3(9, 9, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
 }
