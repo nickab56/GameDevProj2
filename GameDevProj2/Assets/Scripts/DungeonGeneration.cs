67d9ca4084298b21ee8efb67952047666a575b0a
FrozenScorpio <116769282+FrozenScorpio@users.noreply.github.com>
2022-11-28 19:20:58 -0500

    Changes with rooms

--- GameDevProj2/Assets/Scripts/DungeonGeneration.cs
+++ GameDevProj2/Assets/Scripts/DungeonGeneration.cs
@@ -30,31 +30,31 @@ public class DungeonGeneration : MonoBehaviour
     void Start()
     {
         RoomOne = Instantiate(RoomCornerLeft);
-        RoomOne.transform.position = new Vector3(-9, 9, 0);
+        RoomOne.transform.position = new Vector3(-80, 80, 0);
 
         RoomTwo = Instantiate(Room1111);
-        RoomTwo.transform.position = new Vector3(0, 9, 0);
+        RoomTwo.transform.position = new Vector3(0, 80, 0);
 
         RoomThree = Instantiate(RoomCornerBasic);
-        RoomThree.transform.position = new Vector3(9, 9, 0);
+        RoomThree.transform.position = new Vector3(80, 80, 0);
 
         RoomFour = Instantiate(Room1110);
-        RoomFour.transform.position = new Vector3(-9, 0, 0);
+        RoomFour.transform.position = new Vector3(-80, 0, 0);
 
         RoomFive = Instantiate(Room0101);
         RoomFive.transform.position = Vector3.zero;
 
-        RoomEight = Instantiate(RoomBroken1011);
-        RoomEight.transform.position = new Vector3(9, 0, 0);
+        RoomSix = Instantiate(RoomBroken1011);
+        RoomSix.transform.position = new Vector3(80, 0, 0);
 
         RoomSeven = Instantiate(RoomBottomLeftCorner);
-        RoomSeven.transform.position = new Vector3(-9, -9, 0);
+        RoomSeven.transform.position = new Vector3(-80, -80, 0);
 
         RoomEight = Instantiate(RoomCircle1111);
-        RoomEight.transform.position = new Vector3(0, -9, 0);
+        RoomEight.transform.position = new Vector3(0, -80, 0);
 
         RoomNine = Instantiate(RoomBottomRightCorner);
-        RoomNine.transform.position = new Vector3(9, -9, 0);
+        RoomNine.transform.position = new Vector3(80, -80, 0);
 
       
 
