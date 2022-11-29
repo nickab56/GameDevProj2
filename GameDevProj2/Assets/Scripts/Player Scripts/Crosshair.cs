c668c125bb02d5861943413aac3f2d33698202f0
nickab56 <nicholas.abegg@stvincent.edu>
2022-11-28 22:04:23 -0500

    Update Crosshair.cs

--- GameDevProj2/Assets/Scripts/Player Scripts/Crosshair.cs
+++ GameDevProj2/Assets/Scripts/Player Scripts/Crosshair.cs
@@ -12,21 +12,33 @@ public class Crosshair : MonoBehaviour
     private float angle;
     private float speed = 5f;
 
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public Vector3 desiredPosition;
+    public float radius;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        transform.position.Set(transform.position.x, transform.position.y - .25f, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // * Use this to have something rotate towards a target. Would be good for the boss.
-        //direction = target.position - transform.position;
+        
 
-        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed);
+        Vector3 playerToCursor = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.transform.position;
+
+        Vector3 dir = playerToCursor.normalized;
+
+        Vector3 cursorVector = dir * radius;
+
+        transform.position = player.transform.position + cursorVector;
+
     }
+
 }
